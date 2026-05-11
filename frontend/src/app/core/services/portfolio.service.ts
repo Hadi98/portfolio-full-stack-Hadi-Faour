584ed55e77fb3b174fac625db684304530447d77
Hadi98 <faourhadi0@gmail.com>
2026-05-11 03:31:54 +0200

    Fix Angular production build

--- frontend/src/app/core/services/portfolio.service.ts
+++ frontend/src/app/core/services/portfolio.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Portfolio } from '../../models/portfolio';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
-  private readonly apiUrl = 'http://localhost:8080/api/portfolio';
+  private readonly apiUrl = `${environment.apiUrl}/api/portfolio`;
 
   constructor(private http: HttpClient) {}
 
